aeab008682c7ea63b7afe91c02aec4748e7a6685
FB99FIRE <151029053+FB99FIRE@users.noreply.github.com>
2023-11-28 11:12:49 +0100

    change application title (#1)
    Co-authored-by: FB99FIRE <felixbaeten99@gmail.com>

--- src/main/java/com/example/codecadebamie/HelloApplication.java
+++ src/main/java/com/example/codecadebamie/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
+        stage.setTitle("Goodbye!");
         stage.show();
     }
 
